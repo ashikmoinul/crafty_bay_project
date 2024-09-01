ad6fcfcea3c3ea2960e6b8ce3d9d5198b9b21c66
ashik <ashik.moinul150@gmail.com>
2024-09-01 23:54:47 +0600

    Add Splash Screen and ui design

---
+++ lib/app.dart
@@ -0,0 +1,22 @@
+import 'package:crafty_bay_project/presentation/ui/screens/splash_screen.dart';
+import 'package:crafty_bay_project/presentation/ui/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CraftyBayApp extends StatelessWidget {
+  const CraftyBayApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: const SplashScreen(),
+      theme: ThemeData(
+        colorSchemeSeed: AppColors.themeColor,
+        scaffoldBackgroundColor: Colors.white,
+        progressIndicatorTheme: const ProgressIndicatorThemeData(
+          color: AppColors.themeColor,
+
+        )
+      ),
+    );
+  }
+}
